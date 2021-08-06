d65636e1a56dfec08cc7c96f5e428e51bb2ff5d2
ItsMateo <mateotheofficial@gmail.com>
2021-08-05 20:21:18 -0400

    feat(client): improve temporary explore page

--- packages/vindigo-client/src/views/explorer/ExplorerPage.vue
+++ packages/vindigo-client/src/views/explorer/ExplorerPage.vue
@@ -1,7 +1,34 @@
 <template>
 	<section class="explorer-page">
 		<toolbar class="pl-0" />
+		<div class="explore">
+			This is the explore page
+		</div>
 
-		Test
+		<span class="homeButton">
+			<w-button
+				class="ml-0"
+				color="white"
+				bg-color="accent-1"
+				:route="'/'"
+			>
+				Return Home
+			</w-button>
+		</span>
 	</section>
 </template>
+
+<style scoped>
+.homeButton {
+	display: flex;
+	justify-content: center;
+	margin-top: 100px;
+}
+.explore {
+	margin-top: 20px;
+	font-weight: bold;
+	font-size: 30px;
+	display: flex;
+	justify-content: center;
+}
+</style>
