7171fd5aa266e6e178a374856944c9d4efb5f818
macjuul <julian.exodius@gmail.com>
2021-08-08 16:49:11 +0200

    feat: prevent dist deletion on build failure

--- packages/vindigo-cli/gulpfile.js
+++ packages/vindigo-cli/gulpfile.js
@@ -1,17 +1,14 @@
 const {series, dest} = require('gulp');
 const ts = require('gulp-typescript');
 const sm = require('gulp-sourcemaps');
-const rimraf = require('rimraf');
 const path = require('path');
+const { ncp } = require('ncp');
+const fs = require('fs');
 
 // Declare constants
 const tsProject = ts.createProject('tsconfig.json');
 const distDir = path.join(__dirname, 'dist');
-
-// Cleaning the distribution files
-function clean(cb) {
-	rimraf(distDir, cb);
-}
+const distTempDir = path.join(__dirname, 'dist-temp');
 
 // Typescript source files
 function typescript() {
@@ -19,8 +16,30 @@ function typescript() {
 		.pipe(sm.init())
 		.pipe(tsProject())
 		.pipe(sm.write())
-		.pipe(dest(distDir));
+		.pipe(dest(distTempDir));
 }
 
-exports.clean = clean;
-exports.default = series(clean, typescript);
+// Clean the distribution files
+function cleanDist(cb) {
+	if(fs.existsSync(distDir)) {
+		fs.rmdir(distDir, { recursive: true }, cb);
+	} else {
+		cb();
+	}
+}
+
+// Save the distribution files
+function moveCache(cb) {
+	ncp(distTempDir, distDir, cb);
+}
+
+// Finish the compilation process
+function clearCache(cb) {
+	if(fs.existsSync(distTempDir)) {
+		fs.rmdir(distTempDir, { recursive: true }, cb);
+	} else {
+		cb();
+	}
+}
+
+exports.default = series(clearCache, typescript, cleanDist, moveCache, clearCache);
