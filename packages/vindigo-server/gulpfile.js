b5af13fc92f59e7fa9e1b254a794e7bfbce2ab9c
macjuul <julian.exodius@gmail.com>
2021-08-08 16:49:11 +0200

    feat: prevent dist deletion on build failure

--- packages/vindigo-server/gulpfile.js
+++ packages/vindigo-server/gulpfile.js
@@ -1,17 +1,14 @@
 const {series, parallel, src, dest} = require('gulp');
 const ts = require('gulp-typescript');
 const sm = require('gulp-sourcemaps');
-const rimraf = require('rimraf');
 const path = require('path');
+const fs = require('fs');
+const { ncp } = require('ncp');
 
 // Declare constants
 const tsProject = ts.createProject('tsconfig.json');
 const distDir = path.join(__dirname, 'dist');
-
-// Cleaning the distribution files
-function clean(cb) {
-	rimraf(distDir, cb);
-}
+const distTempDir = path.join(__dirname, 'dist-temp');
 
 // Typescript source files
 function typescript() {
@@ -19,13 +16,35 @@ function typescript() {
 		.pipe(sm.init())
 		.pipe(tsProject())
 		.pipe(sm.write())
-		.pipe(dest(distDir));
+		.pipe(dest(distTempDir))
 }
 
 // Copy graphql schemas
 function graphql() {
-	return src("src/**/*.graphql").pipe(dest(distDir));
+	return src("src/**/*.graphql").pipe(dest(distTempDir));
 }
 
-exports.clean = clean;
-exports.default = series(clean, parallel(typescript, graphql));
+// Clean the distribution files
+function cleanDist(cb) {
+	if(fs.existsSync(distDir)) {
+		fs.rmdir(distDir, { recursive: true }, cb);
+	} else {
+		cb();
+	}
+}
+
+// Save the distribution files
+function moveCache(cb) {
+	ncp(distTempDir, distDir, cb);
+}
+
+// Finish the compilation process
+function clearCache(cb) {
+	if(fs.existsSync(distTempDir)) {
+		fs.rmdir(distTempDir, { recursive: true }, cb);
+	} else {
+		cb();
+	}
+}
+
+exports.default = series(clearCache, parallel(typescript, graphql), cleanDist, moveCache, clearCache);
